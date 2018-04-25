86ce881f612fe98e5a6e998fbe0dcc6e4162a35e
Mari Leonhardsen <marileonhardsen@Maris-MacBook-Air.local>
2018-04-25 12:23:09 -0700

    keys

--- earthquake/settings.py
+++ earthquake/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xa1#v%3cnbdy^-dcarbsf3n2l@ynr45)0%9^-u6n^%(w4%88%&'
-#import os
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+#SECRET_KEY = 'xa1#v%3cnbdy^-dcarbsf3n2l@ynr45)0%9^-u6n^%(w4%88%&'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 
 ALLOWED_HOSTS = [
